33996bf73b427864ca7d6a3139b83c1a04a6e89c
enneiymyhamza06-wq <enneiymyhamza06@gmail.com>
2026-01-20 12:05:09 +0100

    start sec_5

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+let carosil1 = document.querySelector('aaa');
+let carosil2 = document.querySelector('bb');
+let carosil3 = document.querySelector('cc');
+let carosil4 = document.querySelector('dd');
+let prev = document.querySelector('prev');
+let next = document.querySelector('next');
+
+next.addEventListener('click',()=>{
+    carosil1
+})
